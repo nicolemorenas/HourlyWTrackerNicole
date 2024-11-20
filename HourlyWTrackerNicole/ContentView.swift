c4a814326b18f1285e4ce9500d6ad1a27fc4229d
nicolemorenas <morenas@shernet.sheridancollege.ca>
2024-11-20 16:18:36 -0500

    Initial Commit

---
+++ HourlyWTrackerNicole/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HourlyWTrackerNicole
+//
+//  Created by Nicole Morenas on 2024-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HourlyWTrackerNicole/HourlyWTrackerNicoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyWTrackerNicoleApp.swift
+//  HourlyWTrackerNicole
+//
+//  Created by Nicole Morenas on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct HourlyWTrackerNicoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
